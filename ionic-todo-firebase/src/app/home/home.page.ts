0119ac51295a0c2755b7e0c9d9d30bfc41941660
Jay Koutavas <jay@heynow.com>
2019-03-31 19:21:43 -0400

    Added a comment showing the difference between the tutorial's access to tasks vs. the latest angularfire2 5.1.2 way of doing things

--- ionic-todo-firebase/src/app/home/home.page.ts
+++ ionic-todo-firebase/src/app/home/home.page.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+/*
+This is how the https://www.javascripttuts.com/using-firebase-and-angularfire2-in-an-ionic-real-time-todo-application/
+tutorial set things up, which is obsolete with version 5.1.2 of angularfire2
+
+import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+
+export class HomePage {
+  tasks: FirebaseListObservable<any[]>;
+
+  constructor(public db: AngularFireDatabase) {
+    this.tasks = db.list('/tasks');
+  }
+}
+*/
+
 import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 import { Observable } from 'rxjs';
 
