a6a2c5013152986b133ca64dfadee0ff8556a96c
Jay Koutavas <jay@heynow.com>
2019-03-31 18:50:59 -0400

    Properly setup home.pages.ts, which differs from the tutorial at https://www.javascripttuts.com/using-firebase-and-angularfire2-in-an-ionic-real-time-todo-application/

--- ionic-todo-firebase/src/app/home/home.page.ts
+++ ionic-todo-firebase/src/app/home/home.page.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tasks: Observable<any[]>;
 
+  constructor(public db: AngularFireDatabase) {
+    this.tasks = db.list('/tasks').valueChanges();
+  }
 }
