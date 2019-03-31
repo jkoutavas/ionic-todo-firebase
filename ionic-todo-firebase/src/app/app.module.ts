764a36b865d18b333f0d6f00d25ef079161be120
Jay Koutavas <jay@heynow.com>
2019-03-31 18:50:04 -0400

    Integrated AngularFire2 and Firebase packages

--- ionic-todo-firebase/src/app/app.module.ts
+++ ionic-todo-firebase/src/app/app.module.ts
@@ -9,14 +9,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
+import {AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebase), AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AngularFireDatabase
   ],
   bootstrap: [AppComponent]
 })
